fdb2e4ae551f58c638bdc6b98ff45522d238b905
DMacPerez <diego.maciel@bairesdev.com>
2021-09-29 19:04:52 -0500

    references updated, tsconfig updated, using Frame.topmost

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./node_modules/@nativescript/types/ios.d.ts" />
+/// <reference path="./node_modules/@nativescript/types/android.d.ts" />
 /// <reference path="./platforms/ios/typings/objc!SwiftRater.d.ts" />
 /// <reference path="./platforms/android/typings/android-rate.d.ts" />
